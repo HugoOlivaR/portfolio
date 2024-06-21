e3f580f0a630540171b9183bf5e4182876f5be48
hugoolv <hugoliva158@gmail.com>
2024-06-21 23:58:19 +0200

    falta pagina proyectos

---
+++ src/i18n/ui.ts
@@ -0,0 +1,112 @@
+export const languages = {
+  en: 'Enslish',
+  es: 'Español',
+};
+
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    "intro": "Hello 👋",
+    "presentation": "I'm Hugo Oliva",
+    "description": "Frontend Developer with a strong passion for details. Specialized in React and committed to excellence in every project.",
+    "home": "Home",
+    "skills": "Skills",
+
+    "experience-title": "Experience",
+    "experience.0.title": "Frontend Developer",
+    "experience.0.company": "Gorilast, Madrid - Spain",
+    "experience.0.date": "March 2024 - June 2024",
+    "experience.0.description1": "- Frontend of a food marketplace.",
+    "experience.0.description2": "- Continuous integration and continuous deployment (CI/CD) using GitLab.",
+    "experience.0.description3": "- Testing with Cypress & Storybook.",
+
+
+    "experience.1.title": "System Administrator",
+    "experience.1.company": "HDT, Madrid - Spain",
+    "experience.1.date": "March 2022 - June 2022",
+    "experience.1.description1": "- Maintained windows servers.",
+    "experience.1.description2": "- Learned and applied VMware for virtualization.",
+    "experience.1.description3": "- Configured and managed NGINX servers.",
+    "experience.1.description4": "- Gained experience in networks and server infrastructure.",
+
+
+
+    "projects-title": "Projects",
+    "projects.0.title": "SparkCollab",
+    "projects.0.description": "Full-stack application designed to facilitate collaboration among students, entrepreneurs, and self-taught individuals. Thanks to its modern stack, SparkCollab ensures a fast, responsive, and seamless user experience.",
+
+    "projects.1.title": "Zentunes",
+    "projects.1.description": "Simple web application that allows users listen some relaxing sounds such as rain, fire, waves and forest. It was developed using vanilla JavaScript, HTML and CSS.",
+
+    "see-more": "See more",
+
+
+    "education-title": "Education",
+    "education.0.title": "AI Master's Degree",
+    "education.0.date": "2023 - 2024",
+    "education.0.description": "Artificial Intelligence and Big Data Master's Degree endorsed by the University of Murcia Spain.",
+
+    "education.1.title": "Application Development Degree",
+    "education.1.date": "2022 - 2024",
+    "education.1.description": "Certificate of Higher Education cross-platform application development.",
+
+    "education.2.title": "Sistems and Networks Degree",
+    "education.2.date": "2020 - 2022",
+    "education.2.description": "Vocational Education and Training Microcomputer systems and networks.",
+    
+    "education.3.title": "Always learning",
+    "education.3.description": "Constantly learning new things to feed my knowledge and let me learn."
+  },
+  es: {
+    "intro": "Hola 👋",
+    "presentation": "Soy Hugo Oliva",
+    "description": "Desarrollador Frontend con una fuerte pasión por los detalles. Especializado en React y comprometido con la excelencia en cada proyecto.",
+    "home": "Inicio",
+    "skills": "Habilidades",
+
+    "experience-title": "Experiencia",
+    "experience.0.title": "Desarrollador Frontend",
+    "experience.0.company": "Gorilast, Madrid - España",
+    "experience.0.date": "Marzo 2024 - Junio 2024",
+    "experience.0.description1": "- Frontend de un marketplace de comida.",
+    "experience.0.description2": "- Integración continua y despliegue continuo (CI/CD) usando GitLab.",
+    "experience.0.description3": "- Testing con Cypress & Storybook.",
+
+    "experience.1.title": "Administrador de Sistemas",
+    "experience.1.company": "HDT, Madrid - España",
+    "experience.1.date": "Marzo 2022 - Junio 2022",
+    "experience.1.description1": "- Mantenimiento de servidores Windows.",
+    "experience.1.description2": "- Uso de VMware para virtualización.",
+    "experience.1.description3": "- Configuración y gestión de servidores NGINX.",
+    "experience.1.description4": "- Experiencia en redes e infraestructura de servidores.",
+
+
+    "projects-title": "Proyectos",
+    "projects.0.title": "SparkCollab",
+    "projects.0.description": "Aplicación full-stack diseñada para facilitar la colaboración entre estudiantes, emprendedores y autodidactas. Gracias a su stack moderno, SparkCollab garantiza una experiencia de usuario rápida, receptiva y sin problemas.",
+
+    "projects.1.title": "Zentunes",
+    "projects.1.description": "Sencilla aplicación web que permite a los usuarios escuchar algunos sonidos relajantes como la lluvia, el fuego, las olas o el bosque. Fue desarrollado usando JavaScript, HTML y CSS.",
+
+    "see-more": "Ver más",
+
+
+    "education-title": "Educación",
+    "education.0.title": "Master en IA",
+    "education.0.date": "2023 - 2024",
+    "education.0.description": "Master en Inteligencia Artificial y Big Data avalado por la Universidad de Murcia España.",
+
+    "education.1.title": "Grado en Desarrollo de Aplicaciones Multiplataforma",
+    "education.1.date": "2022 - 2024",
+    "education.1.description": "Título de Técnico Superior en desarrollo de aplicaciones multiplataforma.",
+
+    "education.2.title": "Grado en Sistemas y Redes",
+    "education.2.date": "2020 - 2022",
+    "education.2.description": "Formación Profesional de Grado Medio en sistemas microinformáticos y redes.",
+
+    "education.3.title": "Siempre aprendiendo",
+    "education.3.description": "Constantemente aprendiendo cosas nuevas para alimentar mi conocimiento y permitirme crecer."
+  },
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        dark: "#0B0B22",
+        dark: "#121212",
+        darkLight: "#1E1E1E",
         light: "#FFFFFF",
         blue: "#13A5FB",
       },
