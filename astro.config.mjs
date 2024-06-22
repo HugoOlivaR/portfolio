7421c507b4c533fba5f599dbf3571190e177202d
hugoolv <hugoliva158@gmail.com>
2024-06-22 08:58:51 +0200

    added action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/portfolio/',
+  site: 'https://hugoolivar.github.io/portfolio/',
   integrations: [tailwind()],
   i18n: {
     locales: ['en', 'es'],
