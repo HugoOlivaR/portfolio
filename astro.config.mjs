24efdb997224dfd7ae8aac94212eecf1d8a10733
hugoolv <hugoliva158@gmail.com>
2024-06-27 20:30:35 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,25 @@
 import { defineConfig } from 'astro/config';
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
 import tailwind from "@astrojs/tailwind";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hugooliva.com/',
-  integrations: [
-    tailwind(),
-    partytown({
-			config: {
-			  forward: ["dataLayer.push"],
-			},
-		}),
-  ],
+  integrations: [tailwind(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), compress(), (await import("astro-compress")).default({
+    CSS: true,
+    JavaScript: true,
+    HTML: true,
+    SVG: true,
+    Image: true,
+  })],
   i18n: {
     locales: ['en', 'es'],
-    defaultLocale: 'en',
-  },
+    defaultLocale: 'en'
+  }
 });
-
